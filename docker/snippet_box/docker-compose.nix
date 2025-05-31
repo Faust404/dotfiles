7236090c0af71c36a5e1cd9a77fddec72bb8503e
Faust404 <himavanth.reddy19@gmail.com>
2025-06-01 02:15:10 +0530

    chore: add extra comments to snippet-box docker-compose.nix to distinguish choice of images for amd/arm devices

--- docker/snippet_box/docker-compose.nix
+++ docker/snippet_box/docker-compose.nix
@@ -6,7 +6,8 @@
     backend = "docker";
     containers = {
       snippet-box = {
-        image = "pawelmalak/snippet-box:arm";
+        image = "pawelmalak/snippet-box:arm"; # For ARM devices
+        # image = "pawelmalak/snippet-box:arm"; # For AMD64 devices
         ports = [ "5000:5000" ];
         volumes = [
           "${config.users.users.faust.home}/docker_volumes/snippet_box/data:/app/data:rw"
