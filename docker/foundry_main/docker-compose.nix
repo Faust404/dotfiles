5fc1658869ab5c4513c170f3670bb1727392e504
Faust404 <himavanth.reddy19@gmail.com>
2025-06-14 11:27:04 +0530

    feat: update foundry to version 13

--- docker/foundry_main/docker-compose.nix
+++ docker/foundry_main/docker-compose.nix
@@ -9,7 +9,7 @@
     backend = "docker";
     containers = {
       foundry_main = {
-        image = "felddy/foundryvtt:12.343";
+        image = "felddy/foundryvtt:13.345";
         ports = [ "30000:30000" ];
         environmentFiles = [ config.age.secrets.foundryvtt.path ];
         volumes = [
