c071d1f41dcf81acdf05c53aa63a43fc42045c14
Faust404 <himavanth.reddy19@gmail.com>
2025-07-14 00:53:39 +0530

    feat: configure basic settings and setup basic applications in home-manager for nostramo-laptop

--- hosts/nostramo/default.nix
+++ hosts/nostramo/default.nix
@@ -1,6 +1,137 @@
-{ inputs, pkgs, ... }:
+{ config, pkgs, ... }:
+
 {
-  # Specify the system architecture  
   nixpkgs.hostPlatform = "x86_64-linux";
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking = {
+    hostName = "nostramo-laptop";
+    networkmanager.enable = true;
+    wireless.enable = false;
+  };
+
+  # Set timeout to 30 minutes
+  security.sudo.extraConfig = ''
+    Defaults  timestamp_timeout=30
+  '';
+
+  # Prevent NetworkManager from being restarted on rebuild
+  systemd.services.NetworkManager.restartIfChanged = false;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.himavanth = {
+    isNormalUser = true;
+    description = "himavanth";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  # Home manager user configuration
+  home-manager.users.himavanth = import ./home.nix;
+  home-manager.backupFileExtension = "backup";
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "himavanth";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    gparted
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+  system.stateVersion = "25.05";
 
 }
---
+++ hosts/nostramo/home.nix
@@ -0,0 +1,199 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.username = "himavanth";
+  home.homeDirectory = "/home/himavanth";
+
+  # Packages to install for your user
+  home.packages = with pkgs; [
+    # Development
+    git
+    vim
+
+    # CLI utilities
+    htop
+    ncdu
+    eza
+    unzip
+    p7zip
+    compose2nix
+
+    # Others
+    vlc
+    zathura
+    libreoffice
+    vesktop
+  ];
+
+  # VS Code config
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+
+    profiles.default = {
+      userSettings = {
+        "window.splashScreen.enabled" = false;
+        "telemetry.telemetryLevel" = "off";
+        "editor.lineNumbers" = "relative";
+        "editor.rulers" = [ 80 120 ];
+        "editor.renderWhitespace" = "boundary";  # Show whitespace at word boundaries
+        "editor.wordWrap" = "off";  # Disable word wrapping to see the rulers better
+      };
+
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        ms-python.python
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        vscodevim.vim
+        charliermarsh.ruff
+        dracula-theme.theme-dracula
+        njpwerner.autodocstring
+        christian-kohler.path-intellisense
+        oderwat.indent-rainbow
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "save-as-root";
+          publisher = "yy0931";
+          version = "1.1.0";
+          sha256 = "lpK4C5zfGEd4jd9XOBkyaw+J2rzpaYTDqyN8ebVDlTo=";
+        }
+      ];
+    };
+  };
+
+  # Git config
+  programs.git = {
+    enable = true;
+    userName = "Faust404";
+    userEmail = "himavanth.reddy19@gmail.com";
+    extraConfig = {
+      credential.helper = "oauth";
+      init.defaultBranch = "main";
+    };
+  };
+
+  # Firefox config
+  programs.firefox = {
+
+    enable = true;
+    profiles.default = {
+
+      search = {
+        force = true;
+        default = "Kagi";
+
+        engines = {
+          "Kagi" = {
+            urls = [
+              {
+                template = "https://kagi.com/search?";
+                params = [
+                  {
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+        };
+      };
+
+      settings = {
+        # Disable about:config warning
+        "browser.aboutConfig.showWarning" = false;
+
+        # Mozilla telemetry
+        "toolkit.telemetry.enabled" = true;
+
+        # Activity Stream
+        "browser.newtab.preload" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.feeds.topsites" = true;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+        "browser.newtabpage.activity-stream.default.sites" = "";
+
+        # Addon recomendations
+        "browser.discovery.enabled" = false;
+        "extensions.getAddons.showPane" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+
+        # Crash reports
+        "breakpad.reportURL" = "";
+        "browser.tabs.crashReporting.sendReport" = false;
+
+        # Auto-decline cookies
+        "cookiebanners.service.mode" = 2;
+        "cookiebanners.service.mode.privateBrowsing" = 2;
+
+        # Disable autoplay
+        "media.autoplay.default" = 5;
+
+        # Prefer dark theme
+        "layout.css.prefers-color-scheme.content-override" = 0; # 0: Dark, 1: Light, 2: Auto
+
+        # HTTPS only
+        "dom.security.https_only_mode" = true;
+
+        # Trusted DNS (TRR)
+        "network.trr.mode" = 2;
+        "network.trr.uri" = "https://mozilla.cloudflare-dns.com/dns-query";
+
+        # ECH - prevent TLS connections leaking request hostname
+        "network.dns.echconfig.enabled" = true;
+        "network.dns.http3_echconfig.enabled" = true;
+
+        # Tracking
+        "browser.contentblocking.category" = "strict";
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.pbmode.enabled" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "privacy.trackingprotection.cryptomining.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+
+        # Fingerprinting
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.resistFingerprinting.pbmode" = true;
+
+        "privacy.firstparty.isolate" = true;
+
+        # URL query tracking
+        "privacy.query_stripping.enabled" = true;
+        "privacy.query_stripping.enabled.pbmode" = true;
+
+        # Prevent WebRTC leaking IP address
+        "media.peerconnection.ice.default_address_only" = true;
+
+        # Use Mozilla geolocation service instead of Google
+        "geo.provider.network.url" = "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%";
+
+        # Disable password manager
+        "signon.rememberSignons" = false;
+        "signon.autofillForms" = false;
+        "signon.formlessCapture.enabled" = false;
+
+        # Hardens against potential credentials phishing:
+        # 0 = don’t allow sub-resources to open HTTP authentication credentials dialogs
+        # 1 = don’t allow cross-origin sub-resources to open HTTP authentication credentials dialogs
+        # 2 = allow sub-resources to open HTTP authentication credentials dialogs (default)
+        "network.auth.subresource-http-auth-allow" = 1;
+      };
+    };
+  };
+
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+
+}
