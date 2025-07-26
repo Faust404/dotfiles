9356d97d8c2f99ed4697d758d42b973ad75d6db9
Faust404 <himavanth.reddy19@gmail.com>
2025-07-27 01:07:57 +0530

    feat: allow gd-helper container to be tracked by watchtower

--- docker/gd_helper/docker-compose.nix
+++ docker/gd_helper/docker-compose.nix
@@ -8,6 +8,9 @@
       gd-helper = {
         image = "index.docker.io/himavanth19/gd-helper:latest";
         ports = [ "5002:5000/tcp" ];
+        labels = {
+          "com.centurylinklabs.watchtower.enable" = "true";
+        };
         log-driver = "journald";
       };
     };
