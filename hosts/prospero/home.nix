6bd77c97e3fa89958c6c0a95c4d0c282c119af9c
Faust404 <himavanth.reddy19@gmail.com>
2025-07-27 00:15:12 +0530

    feat: add some bash aliases to home.nix

--- hosts/prospero/home.nix
+++ hosts/prospero/home.nix
@@ -28,20 +28,54 @@
     };
   };
 
-  # programs.bash = {
-  #   enable = true;
-  #   # bashrcExtra = builtins.readFile ./.bashrc;
-  #   initExtra = builtins.readFile ./.bashrc;
-  # };
-  # # programs.bash.initExtra =  builtins.readFile /home/faust/dotfiles/bashrc;
-  # # home.file.".bashrc".source = ./bashrc;
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      # Control ls output
+      ls = "ls --color=auto";
+      la = "ls -la";
+      l = "ls -CF";
+      "l." = "ls -d .* --color=auto";
 
-  # Example: linking an existing file
-  # ".config/some-app/config".source = ./dotfiles/some-app-config;
+      # Git aliases
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git log --oneline";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. Do not change unless you know what
-  # you're doing.
+      # Navigation
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
+
+      # Do not delete / or prompt if deleting more than 3 files at a time
+      rm = "rm -I --preserve-root";
+      
+      # Confirmation
+      mv = "mv -i";
+      cp = "cp -i";
+      ln = "ln -i";
+
+      # System aliases
+      rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles/.#prospero --show-trace";
+      hm = "home-manager switch";
+      
+      # Docker aliases
+      d = "docker";
+      dc = "docker-compose";
+      dps = "docker ps";
+
+      # Misc
+      grep = "grep --color=auto";
+      fgrep = "fgrep --color=auto";
+      egrep = "egrep --color=auto";
+    };
+
+  };
+
+  # This value determines the Home Manager release that your configuration is compatible with.
   home.stateVersion = "24.11";
 
   # Let Home Manager install and manage itself
