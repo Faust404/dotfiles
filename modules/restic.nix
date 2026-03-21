be806021d01d502001f3b0d2788e9b282b5f4d0e
Faust404 <himavanth.reddy19@gmail.com>
2026-03-21 17:06:22 +0530

    chore: make the username in restic paths dynamic

--- modules/restic.nix
+++ modules/restic.nix
@@ -1,28 +1,32 @@
 { secretsPath, config, pkgs, lib, ... }:
 
-{
 
-  # configure agenix secrets
-  age.secrets = {
-    "restic/repo" = {
-      # file = ../secrets/restic/repo.age;
-      file = secretsPath + /restic/repo.age;
-      owner = "root";
-      mode = "600";
+  let
+    normalUsers = builtins.filter (user: config.users.users.${user}.isNormalUser) (builtins.attrNames config.users.users);
+    defaultUser = if normalUsers != [] then builtins.head normalUsers else "root";
+  in
+  {
+    # configure agenix secrets
+    age.secrets = {
+      "restic/repo" = {
+        # file = ../secrets/restic/repo.age;
+        file = secretsPath + /restic/repo.age;
+        owner = "root";
+        mode = "600";
+      };
+      "restic/password" = {
+        # file = ../secrets/restic/password.age;
+        file = secretsPath + /restic/password.age;
+        owner = "root";
+        mode = "600";
+      };
+      "restic/env" = {
+        # file = ../secrets/restic/env.age;
+        file = secretsPath + /restic/env.age;
+        owner = "root";
+        mode = "600";
+      };
     };
-    "restic/password" = {
-      # file = ../secrets/restic/password.age;
-      file = secretsPath + /restic/password.age;
-      owner = "root";
-      mode = "600";
-    };
-    "restic/env" = {
-      # file = ../secrets/restic/env.age;
-      file = secretsPath + /restic/env.age;
-      owner = "root";
-      mode = "600";
-    };
-  };
 
   # configure restic backup services
   services.restic.backups = {
@@ -34,9 +38,9 @@
       passwordFile = config.age.secrets."restic/password".path;
 
       paths = [
-        "${config.users.users.faust.home}/dotfiles"
-        "${config.users.users.faust.home}/docker_volumes"
-        "${config.users.users.faust.home}/photos"
+        "${config.users.users.${defaultUser}.home}/dotfiles"
+        "${config.users.users.${defaultUser}.home}/docker_volumes"
+        "${config.users.users.${defaultUser}.home}/photos"
       ];
 
       pruneOpts = [
